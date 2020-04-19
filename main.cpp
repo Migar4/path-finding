10fe4f96afb0946d604006ae9396bc222a3c30af
Migara <46416930+Migar4@users.noreply.github.com>
2020-04-19 18:48:50 +1200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
-#include"vec2.h"
+#include <iostream>
 #include "program.h"
-#include "grid.h"
-#include "pawn.h"
+
 
 int main() {
-	program p(10, 10, 25);
+	int m = 0, n = 0, blocks = 0;
+
+	std::cout << "number of rows >>  ";
+	std::cin >> m;
+
+	if (m <= 0) {
+		do {
+			std::cout << "number of rows should be more than 0 >>  ";
+			std::cin >> m;
+		} while (m <= 0);
+	}
+
+	std::cout << "number of columns >>  ";
+	std::cin >> n;
+
+	if (n <= 0) {
+		do {
+			std::cout << "number of columns should be more than 0 >>  ";
+			std::cin >> n;
+		} while (n <= 0);
+	}
+
+	std::cout << "number of blocks >>  ";
+	std::cin >> blocks;
+
+	if (blocks < 0) {
+		do {
+			std::cout << "number of blocks should be more than -1 >>  ";
+			std::cin >> blocks;
+		} while (blocks < 0);
+	}
+
+	program p(m, n, blocks);
 	
 	p.replicate_pawns();
 	p.show_grid();
